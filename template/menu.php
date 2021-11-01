007f100244e441d44b55e3be7935a4732baaf669
oliveiravz <jaovictor.mota@gmail.com>
2021-11-01 13:30:58 -0300

    seg 1 nov

--- template/menu.php
+++ template/menu.php
@@ -143,37 +143,4 @@
             <li><a href="exercicio.php?dir=basico&file=desafio">Desafio</a></li>
         </ul>
     </div>
-    <div class="modulo verde-escuro">
-        <h3>Módulo 06</h3>
-        <ul>
-            <li><a href="">Exercício A</a></li>
-            <li><a href="">Exercício B</a></li>
-            <li><a href="">Exercício C</a></li>
-        </ul>
-    </div>
-    <div class="modulo vermelho-escuro">
-        <h3>Módulo 07</h3>
-        <ul>
-            <li><a href="">Exercício A</a></li>
-            <li><a href="">Exercício B</a></li>
-            <li><a href="">Exercício C</a></li>
-        </ul>
-    </div>
-    <div class="modulo azul-escuro">
-        <h3>Módulo 08</h3>
-        <ul>
-            <li><a href="">Exercício A</a></li>
-            <li><a href="">Exercício B</a></li>
-            <li><a href="">Exercício C</a></li>
-        </ul>
-    </div>
-    <div class="modulo roxo-escuro">
-        <h3>Módulo 09</h3>
-        <ul>
-            <li><a href="">Exercício A</a></li>
-            <li><a href="">Exercício B</a></li>
-            <li><a href="">Exercício C</a></li>
-        </ul>
-
-    </div>
 </nav>
