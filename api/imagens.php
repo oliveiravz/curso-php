7389e3a8ccb0838343d9f707c2cb206074b69f75
oliveiravz <jaovictor.mota@gmail.com>
2021-11-04 09:52:51 -0300

    qui 4 nov

--- api/imagens.php
+++ api/imagens.php
@@ -5,10 +5,12 @@ session_start();
 
 $arquivos = $_SESSION['arquivos'] ?? [];
 
-$nomeArquivo = $_FILES['arquivos']['name'];
-$pastaUpload = __DIR__ . '/../files/';
+// $pastaUpload = __DIR__ . '/../files/';
+$pastaUpload = "exercicio.php?dir=files&file=";
+$nomeArquivo = $_FILES['arquivo']['name'];
 $arquivo = $pastaUpload . $nomeArquivo;
 $tmp = $_FILES['arquivos']['tmp_name'];
+
 
 if (move_uploaded_file($tmp, $arquivo)) {
     echo "<br>Arquivo válido e enviado com sucesso.";
@@ -20,16 +22,16 @@ if (move_uploaded_file($tmp, $arquivo)) {
 ?>
 
 <form action="#" method="post" enctype="multipart/form-data">
-    <input name="arquivos" type="file">
+    <input name="arquivo" type="file">
     <button>Enviar</button>
 </form>
 
 <ul>
     <?php foreach($arquivos as $arquivo){ ?>
-        <?php if(stripos($arquivo, '.png') > 0){ ?>
+        <?php if(stripos($arquivo, '.jpg') > 0){ ?>
             <li>
-                <a href="../files/<?= $arquivo ?>">
-                    <img src="../files/<?= $arquivo ?>" height="120"/>
+                <a href="/../files/<?= $arquivo ?>">
+                    <img src="/../files/<?= $arquivo ?>"/>
                 </a>
             </li>
         <?php } ?>
